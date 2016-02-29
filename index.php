716931579c7a3293b49209783ccaefc8132000a7
Stewart Soutar <sjrsoutar@gmail.com>
2016-02-29 19:18:56 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+	<head>
+		<meta charset="utf-8">
+		<title>Home</title>
+	</head>
+	
+	<body>
+		<script type="text/javascript" src="./StewartsFBapp/fb.js"></script>
+		<div id="content" style="width: 70%; float: left">
+			<h2>Welcome</h2>
+		</div>
+		
+		<div id="sidebar" style="width: 20%; float: right">
+			<div class="fb-login-button" data-scope="public_profile,email" onlogin="checkLoginState();"></div>
+		</div>
+	</body>
+
+</html>
