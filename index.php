267c9895ccc65453420b4a1220851fd0d338e6b4
Stewart Soutar <sjrsoutar@gmail.com>
2016-03-02 08:53:12 +0200

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
 	<body>
 		<script type="text/javascript" src="./StewartsFBapp/fb.js"></script>
 		<div id="content" style="width: 70%; float: left">
-			<h2>Welcome</h2>
+			<h2>Welcome to Balloon Industries</h2>
 		</div>
 		
 		<div id="sidebar" style="width: 20%; float: right">
